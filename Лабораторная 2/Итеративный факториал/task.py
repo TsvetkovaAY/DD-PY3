7e3d2b7b134d43be5c899d57ae4883a509d86355
TsvetkovaAY <tsvetkova.ayu@edu.spbstu.ru>
2023-03-12 03:52:42 +0300

    Лабораторная работа 2, задание "Итеративный факториал" сделано

--- Лабораторная 2/Итеративный факториал/task.py
+++ Лабораторная 2/Итеративный факториал/task.py
@@ -5,4 +5,16 @@ def factorial_iterative(n: int) -> int:
     :param n: Число, факториал которого нужно найти
     :return: n! - факториал числа n
     """
-    ...  # TODO реализовать итеративный алгоритм нахождения факториала
+    .    if not isinstance(n, int):
+        raise TypeError()
+    if n < 0:
+        raise ValueError()
+
+    if n == 0:
+        return 1
+
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+
+    return factorial
