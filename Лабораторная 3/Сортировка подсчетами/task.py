636089bcf3f9d3b42a74f37b4add3cd7241edd43
TsvetkovaAY <tsvetkova.ayu@edu.spbstu.ru>
2023-03-26 23:38:09 +0300

    Задание "Сортировка с подсчетами" из Лабораторной работы №3 выполнено

--- Лабораторная 3/Сортировка подсчетами/task.py
+++ Лабораторная 3/Сортировка подсчетами/task.py
@@ -12,4 +12,18 @@ def sort(container: Sequence[int]) -> Sequence[int]:
     :param container: Массив, который надо отсортировать
     :return: Отсортированный в порядке возрастания массив
     """
-    ...  # TODO реализовать алгоритм сортировки подсчетами
+    n = len(container)
+    if n <= 1:
+        return container
+    max_ = max(container)
+    mas = [0] * (max_ + 1)  # создали вспомогательным массив с нулями
+    for val in container:  # заполнили вспомогательный массив
+        mas[val] += 1
+    container = [0] * n
+    k = 0
+    for i in range(len(mas)):
+        while mas[i] != 0:
+            container[k] = i
+            k += 1
+            mas[i] -= 1
+    return container
